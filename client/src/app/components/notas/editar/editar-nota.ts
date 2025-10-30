b39259df0e0ed885528627bb3508640d3fef3671
coelho-glith <coelho.soldado7@gmail.com>
2025-10-30 15:41:39 -0300

    feat: adicionado pasta editar em notas

--- client/src/app/components/notas/editar/editar-nota.ts
+++ client/src/app/components/notas/editar/editar-nota.ts
@@ -1,10 +1,90 @@
-import { Component } from '@angular/core';
+import { AsyncPipe } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
+import { filter, map, Observer, shareReplay, switchMap, take, tap } from 'rxjs';
+import { ListagemCategoriasModel } from '../../categorias/categoria.models';
+import { NotificacaoService } from '../../shell/notificacao/notificacao.service';
+import { NotaService } from '../nota.service';
+import { DetalhesNotaModel, EditarNotaModel, EditarNotaResponseModel } from '../nota.models';
 
 @Component({
   selector: 'app-editar-nota',
-  imports: [],
-  templateUrl: './editar-nota.html'
+  imports: [
+    MatCardModule,
+    MatButtonModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    RouterLink,
+    AsyncPipe,
+    ReactiveFormsModule,
+  ],
+  templateUrl: './editar-nota.html',
 })
 export class EditarNota {
+  protected readonly formBuilder = inject(FormBuilder);
+  protected readonly route = inject(ActivatedRoute);
+  protected readonly router = inject(Router);
+  protected readonly notaService = inject(NotaService);
+  protected readonly notificacaoService = inject(NotificacaoService);
 
+  protected notaForm: FormGroup = this.formBuilder.group({
+    titulo: ['', [Validators.required, Validators.minLength(3)]],
+    conteudo: [''],
+    categoriaId: ['', [Validators.required]],
+  });
+
+  get titulo() {
+    return this.notaForm.get('titulo');
+  }
+
+  get conteudo() {
+    return this.notaForm.get('conteudo');
+  }
+
+  get categoriaId() {
+    return this.notaForm.get('categoriaId');
+  }
+
+  protected readonly categorias$ = this.route.data.pipe(
+    filter((data) => data['categorias']),
+    map((data) => data['categorias'] as ListagemCategoriasModel[])
+  );
+
+  protected readonly nota$ = this.route.data.pipe(
+    filter((data) => data['nota']),
+    map((data) => data['nota'] as DetalhesNotaModel),
+    tap((nota) => this.notaForm.patchValue({ ...nota, categoriaId: nota.categoria })),
+    shareReplay({ bufferSize: 1, refCount: true })
+  );
+
+  public editar() {
+    if (this.notaForm.invalid) return;
+
+    const editarNotaModel: EditarNotaModel = this.notaForm.value;
+
+    const edicaoObserver: Observer<EditarNotaResponseModel> = {
+      next: () =>
+        this.notificacaoService.sucesso(
+          `O registro "${editarNotaModel.titulo}" foi editado com sucesso!`
+        ),
+      error: (err) => this.notificacaoService.erro(err.message),
+      complete: () => this.router.navigate(['/notas']),
+    };
+
+    this.nota$
+      .pipe(
+        take(1),
+        switchMap((nota) => this.notaService.editar(nota.id, editarNotaModel))
+      )
+      .subscribe(edicaoObserver);
+  }
 }
--- client/src/app/components/notas/nota.routes.ts
+++ client/src/app/components/notas/nota.routes.ts
@@ -1,14 +1,25 @@
-import { Routes } from '@angular/router';
+import { ActivatedRouteSnapshot, Routes } from '@angular/router';
 import { NotaService } from './nota.service';
 import { ListarNotas } from './listar/listar-notas';
 import { inject } from '@angular/core';
 import { CategoriaService } from '../categorias/categoria.service';
 import { CadastrarNota } from './cadastrar/cadastrar-nota';
+import { EditarNota } from './editar/editar-nota';
 
 const listagemNotasResolver = () => {
   const notaService = inject(NotaService);
 
   return notaService.selecionarTodas();
+};
+
+const detalhesNotaResolver = (route: ActivatedRouteSnapshot) => {
+  const notaService = inject(NotaService);
+
+  if (!route.paramMap.has('id')) throw new Error('O parâmetro id não foi fornecido.');
+
+  const notaId = route.paramMap.get('id')!;
+
+  return notaService.selecionarPorId(notaId);
 };
 
 const listagemCategoriasResolver = () => {
@@ -26,6 +37,11 @@ export const notaRoutes: Routes = [
         path: 'cadastrar',
         component: CadastrarNota,
         resolve: { categorias: listagemCategoriasResolver },
+      },
+      {
+        path: 'editar/:id',
+        component: EditarNota,
+        resolve: { nota: detalhesNotaResolver, categorias: listagemCategoriasResolver },
       },
     ],
     providers: [NotaService, CategoriaService],
